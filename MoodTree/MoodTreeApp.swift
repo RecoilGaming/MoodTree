3f30d7eb0206f26c0135284e85017d6a1b158ae0
RecoilGaming <qinzhaolee@gmail.com>
2024-10-27 11:56:42 -0700

    Set up Swift project

---
+++ MoodTree/MoodTreeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoodTreeApp.swift
+//  MoodTree
+//
+//  Created by Qinzhao Li on 10/27/24.
+//
+
+import SwiftUI
+
+@main
+struct MoodTreeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
